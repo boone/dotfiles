22a557a0489423342b05b699fd068f655733c892
Rubem Nakamura <rubem.nakamura@gmail.com>
2012-09-21 19:10:44 -0300

    updating the paths on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 
 echo "Creating symlinks..."
 
-if ! [[ -L $HOME/.vimrc ]]; then
-  ln -s `pwd`/vim/.vimrc $HOME/.vimrc
+if ! [[ -L $HOME/vimrc ]]; then
+  ln -s `pwd`/vimrc $HOME/.vimrc
 fi
 
-if ! [[ -L $HOME/.vim ]]; then
-  ln -s `pwd`/vim/.vim/ $HOME/.vim
+if ! [[ -L $HOME/vim ]]; then
+  ln -s `pwd`/vim/ $HOME/.vim
 fi
 
 echo
 
-if ! [[ -d `pwd`/vim/.vim/bundle/vundle/.git  ]]; then
+if ! [[ -d `pwd`/vim/bundle/vundle/.git  ]]; then
   echo "Cloning Vundle..."
-  git clone https://github.com/gmarik/vundle.git `pwd`/vim/.vim/bundle/vundle
+  git clone https://github.com/gmarik/vundle.git `pwd`/vim/bundle/vundle
 else
   echo "Skipping Vundle clone"
 fi
