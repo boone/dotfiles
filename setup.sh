3f5cd3efa7d6f6ca8e91143b3aa6b673537830b2
Rubem Nakamura <rubem.nakamura@gmail.com>
2015-08-18 14:37:57 -0300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 echo "Setting up VIM"
@@ -28,7 +29,22 @@ fi
 vim +BundleInstall +qall
 vim +BundleClean +qall
 
+echo
+
 echo "Setting up bash environment"
 
-echo "  Linking .bash"
-ln -s $HOME/.bash `pwd`/bash
+if ! [[ -L $HOME/.bash ]]; then
+  echo "  Linking .bash"
+  ln -s `pwd`/bash $HOME/.bash
+else
+  echo "  SKIP - .bash directory"
+fi
+
+if ! [[ -e $HOME/.bashrc ]]; then
+  echo "  Linking .bashrc"
+  ln -s `pwd`/bashrc $HOME/.bashrc
+else
+  echo "  SKIP - .bashrc"
+fi
+
+source $HOME/.bashrc
